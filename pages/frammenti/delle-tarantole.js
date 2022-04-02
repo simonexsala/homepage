902bfbba30630c55586587a1a56b2332dca9b7ff
Simone Sala <39414823+simonexsala@users.noreply.github.com>
2022-04-03 00:27:07 +0200

    Update delle-tarantole.js
    added text

--- pages/frammenti/delle-tarantole.js
+++ pages/frammenti/delle-tarantole.js
@@ -19,8 +19,124 @@ const Frammento = () => {
           </ListItem>
         </List>
         <Paragraph>
-          https://it.wikisource.org/wiki/Cos%C3%AC_parl%C3%B2_Zarathustra/Parte_seconda/Delle_tarantole
-          /// WRITE BASH SCRIPT PRENDE UN TXT E SU OGNI INVIO AGGIUNGE UN NUOVO PARAGRAFO
+          «Ecco, questa è la caverna della tarantola! Vuoi proprio vederla? Qui pende la sua rete: toccala, perchè vibri.
+        </Paragraph>
+        <Paragraph>
+          Eccola che giunge volonterosa: benvenuta tarantola! Nero porti sul dosso il triangolo, tuo simbolo; io so anche che cosa porti nell'anima.
+        </Paragraph>
+        <Paragraph>
+          La tua anima cova la vendetta: dove tu mordi si forma una cancrena nera; il tuo veleno fa che l'anima tua s'aggiri con la vendetta!
+        </Paragraph>
+        <Paragraph>
+          Così parlo per similitudine di voi, che fate girar le anime, di voi predicatori dell'uguaglianza! Voi siete simili alle tarantole, e v'arde un celato desiderio di vendetta!
+        </Paragraph>
+        <Paragraph>
+          Ma io voglio discoprire i vostri nascondigli: perciò vi lancio in volto il riso che viene dall'alto.
+        </Paragraph>
+        <Paragraph>
+          Io lacero dunque la vostra rete, affinchè il vostro furore v'attiri fuori dalla vostra caverna di menzogne, e faccia sprizzar il vostro desiderio di vendetta fuor dalla vostra parola «giustizia».
+        </Paragraph>
+        <Paragraph>
+          Redimer l'uomo dalla vendetta: questo è per me il ponte che guida alla più sublime speranza e l'arcobaleno che brilla dopo lunghe tempeste.
+        </Paragraph>
+        <Paragraph>
+          Ma in altro modo l'intendono le tarantole. «Questo appunto è per noi giustizia: che il mondo sia pieno delle tempeste della nostra vendetta», così parlano esse.
+        </Paragraph>
+        <Paragraph>
+          «Vendetta noi vogliamo e obbrobrio per tutti coloro che non sono uguali a noi» — così giuran le tarantole nel loro cuore.
+        </Paragraph>
+        <Paragraph>
+          La volontà dell'uguaglianza — questo sia di qui innanzi sinonimo di virtù; contro tutto ciò, che è potente, noi vogliamo levare il nostro grido!
+        </Paragraph>
+        <Paragraph>
+          O voi predicatori dell'uguaglianza, la follìa cesarea dell'impotenza è quella che in voi chiede «uguaglianza»: le vostre più occulte brame tiranniche si mascherano di parole virtuose!
+        </Paragraph>
+        <Paragraph>
+          Presunzione arcigna, invidia rattenuta, forse l'orgoglio e l'invidia dei vostri padri, in voi scoppiano come una fiamma, come una manìa di vendetta.
+        </Paragraph>
+        <Paragraph>
+          Ciò che il padre tacque s'esprime nella parola del figlio; e bene spesso trovai essere il figlio il segreto rivelato del padre.
+        </Paragraph>
+        <Paragraph>
+          Essi rassomigliano agli entusiasti: ma non già il cuore ispira il loro entusiasmo, — bensì la vendetta. E quando divengono acuti e freddi, non lo spirito li rende tali, bensì l'invidia.
+        </Paragraph>
+        <Paragraph>
+          Questa sola li conduce anche sui sentieri dei pensatori; e l'indizio della loro invidia è ch'essi vanno sempre troppo lontano: così che quando sono stanchi devono gettarsi a giacere sulla neve. Da tutti i loro lamenti traspare la vendetta, da ogni lor lode il desiderio di recar dolore: ed esser giudici è per essi la maggiore delle felicità.
+        </Paragraph>
+        <Paragraph>
+          Ma questo io insegno a voi, o miei amici; diffidate di tutti coloro, nei quali è potente l'istinto del punire!
+        </Paragraph>
+        <Paragraph>
+          È gente di cattiva indole e di trista specie: ha nel volto l'imagine del carnefice e del segugio.
+        </Paragraph>
+        <Paragraph>
+          Diffidate di coloro che hanno sempre in bocca la giustizia. In verità, alle loro anime fa difetto non il miele soltanto!
+        </Paragraph>
+        <Paragraph>
+          E quando chiamano sè stessi «buoni e i giusti» non dimenticate che per diventar Farise non manca loro che la potenza!
+        </Paragraph>
+        <Paragraph>
+          Miei fratelli, io non voglio essere confuso e scambiato con costoro.
+        </Paragraph>
+        <Paragraph>
+          V'hanno taluni che predicano la mia dottrina della vita, e si fanno insieme sostenitori dell'uguaglianza e delle tarantole.
+        </Paragraph>
+        <Paragraph>
+          Se essi vi parlano in favor della vita, e sono ciò non di meno rintanati nella loro caverna — quei ragni velenosi — appartati dalla vita, credetemi, essi con ciò vogliono recar dolore.
+        </Paragraph>
+        <Paragraph>
+          Essi vogliono recar dolore a quelli che sono ora potenti; giacchè per costoro il sermone della morte trova la più opportuna applicazione.
+        </Paragraph>
+        <Paragraph>
+          Se la cosa fosse altrimenti, essi insegnerebbero altra cosa; in altri tempi costoro furono i peggiori calunniatori della vita e i bruciatori d'eretici.
+        </Paragraph>
+        <Paragraph>
+          Con tali predicatori dell'uguaglianza io non voglio essere confuso o scambiato. Poi che così parlò in me la giustizia: «Gli uomini non sono uguali».
+        </Paragraph>
+        <Paragraph>
+          E nemmeno devono diventare tali! Che cosa sarebbe del mio amore pel superuomo, se io parlassi diversamente? Su mille ponti e sentieri essi devono lanciarsi verso l'avvenire, e sempre più ci dev'essere tra di loro guerra ed ineguaglianza: ciò m'insegna il mio grande amore!
+        </Paragraph>
+        <Paragraph>
+          Inventori di imagini e di spettri essi devono diventare nelle loro inimicizie, e con le loro imagini e i loro spettri debbono combattere tra di loro la più terribile battaglia!
+        </Paragraph>
+        <Paragraph>
+          Bene e male, e ricco e povero, e alto e basso, e tutti i valori, comunque si chiamino, devono essere armi e segnacoli sfolgoranti di questa verità: che la vita deve sempre oltrepassare sè stessa.
+        </Paragraph>
+        <Paragraph>
+          In alto la vita lotta a comporsi un edifizio con pilastri e gradini: essa vuole spaziare per grandi distanze e goder di bellezze beate, — per ciò essa ha bisogno d'elevarsi!
+        </Paragraph>
+        <Paragraph>
+          E perchè ha bisogno dell'altezza, le son necessari i gradini, e le giova il contrasto tra i gradini e coloro che li salgono! La vita vuole salire, e, salendo e sorpassando sè stessa, rigenerarsi.
+        </Paragraph>
+        <Paragraph>
+          E guardate un po', miei amici! Qui, dove si trova la caverna della tarantola, s'ergono le rovine d'un antico tempio, — guardate un po' qui con occhi illuminati!
+        </Paragraph>
+        <Paragraph>
+          In verità, quegli che qui ordinò i suoi pensieri e li tese verso l'alto, conosceva il segreto della vita al pari dell'uomo più saggio!
+        </Paragraph>
+        <Paragraph>
+          Che anche nella bellezza sia lotta e ineguaglianza, e guerra per la potenza e per la superiorità: ecco ciò che egli c'insegna in una similitudine molto chiara.
+        </Paragraph>
+        <Paragraph>
+          Oh! come le vôlte e gli archi s'incurvano divinamente nella lotta a corpo a corpo: come combattono tra loro con luce e tenebre i divini lottatori! — Fate che anche i nostri nemici siano altrettanto belli e securi! Divinamente noi vogliamo lottare l'un contro l'altro!
+        </Paragraph>
+        <Paragraph>
+          Ahimè! Ecco che mi morse la tarantola, la mia antica nemica! Con sicurezza e bellezza divina essa mi morse nel dito!
+        </Paragraph>
+        <Paragraph>
+          «Il castigo e la giustizia devono essere — essa pensa; — non impunemente egli qui potrà inneggiare all'inimicizia!».
+        </Paragraph>
+        <Paragraph>
+          Sì, essa si è vendicata! E guai a me, se ora nella sua vendetta farà turbinare anche la mia anima!
+        </Paragraph>
+        <Paragraph>
+          Ma perchè il vortice non mi avvolga, o miei amici, legatemi fortemente a questa colonna! Preferisco diventare uno stilita, anzi che il vortice della bramosia di vendetta.
+        </Paragraph>
+        <Paragraph>
+          In verità, Zarathustra non è un vento che gira vorticosamente; se bene è un danzatore, non danzerà mai la tarantella!».
+        </Paragraph>
+        <Paragraph>
+          Così parlò Zarathustra.
         </Paragraph>
       </Container>
     </Layout>
