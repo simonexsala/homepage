744d882a84fda907294269ec0aaa015b505efb7a
Simone Sala <39414823+simonexsala@users.noreply.github.com>
2022-04-02 23:30:52 +0200

    Update works.js
    removed items

--- pages/works.js
+++ pages/works.js
@@ -31,26 +31,6 @@ const Works = () => (
           </WorkGridItem>
         </Section>
       </SimpleGrid>
-
-      <Section delay={0.2}>
-        <Divider my={6} />
-        <Heading as="h3" fontSize={20} mb={4}>
-          Collaborations
-        </Heading>
-      </Section>
-
-      <SimpleGrid columns={[1,1,2]} gap={6}>
-        <Section delay={0.3}>
-          <WorkGridItem id="template" title="Template" thumbnail={thumbnailPEPE}>
-            toni
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.3}>
-          <WorkGridItem id="uaaa" title="Chiara" thumbnail={thumbnailPEPE}>
-            La mia bellissima scimmietta ❤️
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
     </Container>
   </Layout>
 )
