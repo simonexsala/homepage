11796f73ba6506a2968463b9f3b581f4cd4651b4
Simone Sala <simonexsala@protonmail.com>
2022-04-09 18:06:51 +0200

    new writing

--- pages/works.js
+++ pages/works.js
@@ -17,7 +17,13 @@ const Works = () => (
       </Heading>
 
       <SimpleGrid columns={[1,1,2]} gap={6}>
-<<<<<<< Updated upstream
+        <Section delay={0.1}>
+          <WorkGridItem id="nuova-frontiera" title="La nuova frontiera della società di controllo" thumbnail={thumbanilCONTROLLO}>
+            Il furto della sovranità individuale
+            <br></br>
+            <Badge>23 Marzo 2022</Badge>
+          </WorkGridItem>
+        </Section>
         <Section delay={0.1}>
           <WorkGridItem id="pepe" title="Pepe e Wojak" thumbnail={thumbnailPEPE}>
             Simboli dell'inconscio
@@ -32,29 +38,6 @@ const Works = () => (
             <Badge>10 Febbraio 2022</Badge>
           </WorkGridItem>
         </Section>
-=======
-      <Section delay={0.1}>
-        <WorkGridItem id="nuova-frontiera" title="La nuova frontiera della società di controllo" thumbnail={thumbanilCONTROLLO}>
-          Il furto della sovranità individuale
-          <br></br>
-          <Badge>23 Marzo 2022</Badge>
-        </WorkGridItem>
-      </Section>
-      <Section delay={0.1}>
-        <WorkGridItem id="pepe" title="Pepe e Wojak" thumbnail={thumbnailPEPE}>
-          Simboli dell'inconscio
-          <br></br>
-          <Badge>23 Marzo 2022</Badge>
-        </WorkGridItem>
-      </Section>
-      <Section delay={0.1}>
-        <WorkGridItem id="quadro-macroeconomico" title="Quadro macroeconomico" thumbnail={thumbnailFED}>
-          Attraverso il passato per il futuro
-          <br></br>
-          <Badge>10 Febbraio 2022</Badge>
-        </WorkGridItem>
-      </Section>
->>>>>>> Stashed changes
       </SimpleGrid>
     </Container>
   </Layout>
